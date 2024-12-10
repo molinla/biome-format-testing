d98cb05df1a14381ce8657a5c3329960b7e28563
molinla <moninla@126.com>
2024-12-11 00:24:10 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+/**
+ * following codes has lots of space, should be removed only left one after formating.
+ * @type {number[]}
+ */
+const formatArray =                                           [1, 2, 3]
+
+/**
+ * Volta error: Could not parse project manifest
+ * at D:\Webstorm\biome-format-testing\package.json
+ *
+ * Please ensure that the file is correctly formatted.
+ * Error details written to C:\Users\moninla\AppData\Local\Volta\log\volta-error-2024-12-11_00_21_57.090.log
+ * StdIO connection closed
+ * Exit code: 126
+ * Command line: "C:\Program Files\Volta\node.exe" D:\Webstorm\biome-format-testing\node_modules\@biomejs\biome\bin\biome lsp-proxy --config-path D:/Webstorm/biome-format-testing
+ */
+
+/**
+ * file: volta-error-2024-12-11_00_21_57.090.log
+ * "C:\\Program Files\\Volta\\node.exe" "D:\\Webstorm\\biome-format-testing\\node_modules\\@biomejs\\biome\\bin\\biome" "lsp-proxy" "--config-path" "D:/Webstorm/biome-format-testing"
+ * Volta v1.1.1
+ *
+ * Could not parse project manifest
+ * at D:\Webstorm\biome-format-testing\package.json
+ *
+ * Please ensure that the file is correctly formatted.
+ *
+ * Error cause: EOF while parsing a value at line 1 column 0
+ */
